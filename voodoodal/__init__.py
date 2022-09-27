a642d030ef4848f165fdf12fd6711b8279078dcb
Val K <valq7711@gmail.com>
2022-09-27 14:49:42 +0300

    bump version and make release

--- voodoodal/__init__.py
+++ voodoodal/__init__.py
@@ -12,4 +12,4 @@ __all__ = (
 __author__ = "Valery Kucherov <valq7711@gmail.com>"
 __copyright__ = "Copyright (C) 2022 Valery Kucherov"
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
